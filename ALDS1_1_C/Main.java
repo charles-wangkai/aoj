3c08bb012979a36954595c2b4f137a25e1d0cdea
Kai Wang <charles.wangkai@gmail.com>
2024-06-17 11:20:46 +0800

    Prime Numbers

---
+++ ALDS1_1_C/Main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] a = new int[N];
+    for (int i = 0; i < a.length; ++i) {
+      a[i] = sc.nextInt();
+    }
+
+    System.out.println(solve(a));
+
+    sc.close();
+  }
+
+  static int solve(int[] a) {
+    return (int) Arrays.stream(a).filter(Main::isPrime).count();
+  }
+
+  static boolean isPrime(int x) {
+    for (int i = 2; i * i <= x; ++i) {
+      if (x % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
